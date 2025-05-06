23a3d2f943f7bde21b60da61cce2d575e549e6c2
TrottingFoxStudio <trottingfoxstudio@gmail.com>
2025-05-05 21:35:35 -0400

    First Commit - Step 1

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿namespace _1011OOP_Wk1Class1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Happy New Year, 2024!!");
+    }
+}
---
+++ lectureBasedChallenge.cs
@@ -0,0 +1,21 @@
+/*namespace _1011OOP_Wk1Class1;
+
+public class lectureBasedChallenge
+{
+    string userName = "";
+    int currentYear = 0;
+    int userYear = 1989;
+        
+        
+    Console.WriteLine("What's your name?");
+    userName = Console.ReadLine();
+        
+    Console.WriteLine("What year is the year?");
+    currentYear = int.Parse(Console.ReadLine());
+        
+        
+        for (int i  = 1989; i <= currentYear; i++)
+    Console.WriteLine("Happy New Year " + userName + ", " + currentYear + "!!");
+
+
+}*/
