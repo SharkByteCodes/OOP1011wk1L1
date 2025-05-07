4c58c97e1debf8977192ec33559ea231b0db9274
TrottingFoxStudio <trottingfoxstudio@gmail.com>
2025-05-06 23:16:42 -0400

    Exercise 1, Step 2.

--- Program.cs
+++ Program.cs
@@ -2,8 +2,17 @@
 
 class Program
 {
+    
+//    Step 2 - Variables & assignment statements:
+//    Change the program so that it uses two variables - one for your name, and one for the year - and prints "Happy New Year (yourname), (the year)!!".
     static void Main(string[] args)
     {
-        Console.WriteLine("Happy New Year, 2024!!");
+        
+        // Declare name and current year in variables
+        string userName = "SharkByteCodes"; // github usernme for privacy
+        int currentYear = 2025;
+        
+        //
+        Console.WriteLine("Happy New Year " + userName + ", " + currentYear + "!!");
     }
 }
--- README.md
+++ README.md
@@ -3,4 +3,6 @@ Object-Oriented Programming - GME1011 Week 1 Lesson 1
 
 I dont know what to write in these read mes.
 
+Commit History should show/declare various stesps in the exercises.
+
 >> Cretive commons to do whatever you want with the code. this is here to show breadcrumbs (as it were) that I learned something in class.
