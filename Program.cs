3cc01f4c0c058c0754bdf84f802936c55611129f
TrottingFoxStudio <trottingfoxstudio@gmail.com>
2025-05-06 23:50:35 -0400

    Exercise 1, Step 4

--- Program.cs
+++ Program.cs
@@ -2,27 +2,34 @@
 
 class Program
 {
-/*    Getting batter at comments..
- 
-   Step 3 - User input:
-   Change the program so that both the name and year are collected through user-input (don't forget your prompts).
+/*    Getting batter at comments.. << batter???
+   Step 4 - Loops:
+   Change the program so that it prints "Happy New Year (yourname), (the year)!!" from the year you were born until the year the user entered. Something like this:
+   
+  ... clipped ...
 */
     static void Main(string[] args)
     {
         
-        // Declare name and current year in variables
+        // Declare name, user birth year and current year in variables
+        // int changed to double for more precision, down the line
         string userName = ""; // empty string, not null (I think).
-        int currentYear = 0; // non-magical number.
+        double userBirthYear = 0; // non-magical number.
+        double currentYear = 0; // non-magical number.
         
         // Prompt user for username
         Console.WriteLine("What's your name?");
-        userName = Console.ReadLine(); // collect user input
+        userName = Console.ReadLine(); // collect user input: name
+        Console.WriteLine("What's your birth year?");
+        userBirthYear = double.Parse(Console.ReadLine()); // collect user birth year
         
         // Prompt user for year
         Console.WriteLine("What's the current year?");
-        currentYear = int.Parse(Console.ReadLine()); // Parse user input (string) int.
-        
-        
-        Console.WriteLine("Happy New Year " + userName + ", " + currentYear + "!!");
+        currentYear = double.Parse(Console.ReadLine()); // Parse user input (string) int.
+
+        for (double i = userBirthYear; i <= currentYear; i++)
+        {
+            Console.WriteLine("Happy New Year " + userName + ", " + currentYear + "!!");
+        }
     }
 }
