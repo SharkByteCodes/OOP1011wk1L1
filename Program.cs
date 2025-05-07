08915393d09ab876a9a0388ba0d2abc5930e2453
TrottingFoxStudio <trottingfoxstudio@gmail.com>
2025-05-07 01:14:52 -0400

    Exercise 1, Step 6 (Methods)

--- Program.cs
+++ Program.cs
@@ -2,32 +2,38 @@
 
 class Program
 {
-/*   Leap years. I never understood modern calendars, or ancient ones..
-   Step 5 - If-statements:
-   Change the program so that leap-years are somehow highlighted!!!
-   
-... clipped ...
+/*   Methods
+   Step 6 (optional) - Methods:
+   Change the program to use at least one method.
 */
-    static void Main(string[] args)
+    public static void Main(string[] args)
     {
-        
+        loopYear(); // TODO: what is this mess? I have 10 warnings..
+
+
+    }
+
+    static string loopYear()
+    {
+
         // Declare name, user birth year and current year in variables
         // int changed to double for more precision, down the line
         string userName = ""; // empty string, not null (I think).
         double userBirthYear = 0; // non-magical number.
         double currentYear = 0; // non-magical number.
         double leapYear = 4; // used for checking if year is divisable by 4 (leap year).
-        
+
         // Prompt user for username
         Console.WriteLine("What's your name?");
         userName = Console.ReadLine(); // collect user input: name
         Console.WriteLine("What's your birth year?");
         userBirthYear = double.Parse(Console.ReadLine()); // collect user birth year
-        
+
         // Prompt user for year
         Console.WriteLine("What's the current year?");
         currentYear = double.Parse(Console.ReadLine()); // Parse user input (string) int.
 
+        // print to console for a while
         for (int i = Convert.ToInt16(userBirthYear); i <= currentYear; i++) // convert to int 16 is sloppy but works
         {
             Console.Write("Happy New Year " + userName + ", " + i + "!!");
@@ -35,8 +41,14 @@ class Program
             {
                 Console.Write(" leap year, yay!");
             }
+
+            // set a new line
             Console.WriteLine(""); //also sloppy, but works.
 
         }
+
+        return null; // TODO: learn why this is is a bad idea.
     }
+   
 }
+
