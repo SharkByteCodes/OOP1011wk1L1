0c22b762131b52a76f8f0d273e078db33db7c36d
TrottingFoxStudio <trottingfoxstudio@gmail.com>
2025-05-07 00:47:26 -0400

    Exercise 1, Step 5 (if statements)

--- Program.cs
+++ Program.cs
@@ -2,11 +2,18 @@
 
 class Program
 {
-/*    Getting batter at comments.. << batter???
-   Step 4 - Loops:
-   Change the program so that it prints "Happy New Year (yourname), (the year)!!" from the year you were born until the year the user entered. Something like this:
+/*   Leap years. I never understood modern calendars, or ancient ones..
+   Step 5 - If-statements:
+   Change the program so that leap-years are somehow highlighted!!!
    
-  ... clipped ...
+   Happy New Year Aaron, 1976!! (leap year, yay!)
+   Happy New Year Aaron, 1977!!
+   Happy New Year Aaron, 1978!!
+   ...
+   Happy New Year Aaron, 2021!! 
+   Happy New Year Aaron, 2022!!
+   Happy New Year Aaron, 2023!!
+   Happy New Year Aaron, 2024!! (leap year, yay!)
 */
     static void Main(string[] args)
     {
@@ -16,6 +23,7 @@ class Program
         string userName = ""; // empty string, not null (I think).
         double userBirthYear = 0; // non-magical number.
         double currentYear = 0; // non-magical number.
+        double leapYear = 4; // used for checking if year is divisable by 4 (leap year).
         
         // Prompt user for username
         Console.WriteLine("What's your name?");
@@ -27,9 +35,15 @@ class Program
         Console.WriteLine("What's the current year?");
         currentYear = double.Parse(Console.ReadLine()); // Parse user input (string) int.
 
-        for (double i = userBirthYear; i <= currentYear; i++)
+        for (int i = Convert.ToInt16(userBirthYear); i <= currentYear; i++) // convert to int 16 is sloppy but works
         {
-            Console.WriteLine("Happy New Year " + userName + ", " + currentYear + "!!");
+            Console.Write("Happy New Year " + userName + ", " + i + "!!");
+            if (DateTime.IsLeapYear(i))
+            {
+                Console.Write(" leap year, yay!");
+            }
+            Console.WriteLine(""); //also sloppy, but works.
+
         }
     }
 }
